b6db65cff0ef157ade58d4f15fdd3a4dd72fdfee
slinky55 <larry.mason@ufl.edu>
2024-09-01 14:12:32 -0400

    Big restart

--- AsyncWebCamera.h
+++
@@ -1,9 +0,0 @@
-#ifndef DING_ASYNC_CAM_H
-#define DING_ASYNC_CAM_H
-
-void getCameraStatus(AsyncWebServerRequest *request);
-void setCameraVar(AsyncWebServerRequest *request);
-void streamJpg(AsyncWebServerRequest *request);
-void sendJpg(AsyncWebServerRequest *request);
-
-#endif
